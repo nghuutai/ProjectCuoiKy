2fc8cf4361730c8d45448f1a5ffbf27caf42d0c0
Nguyen Huu Tai <nhtai12435@gmail.com>
2019-11-07 16:45:34 +0700

    tao file databasecauhinh

--- src/main/java/controllers/ChiTietSanPhamController.java
+++ src/main/java/controllers/ChiTietSanPhamController.java
@@ -1,13 +1,22 @@
 package controllers;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import dao.DatabaseSanPham;
+import entity.SanPham;
 
 @Controller
 public class ChiTietSanPhamController {
 
 	@RequestMapping("/chitietsanpham/{id}")
-	public String chiTietSanPham() {
+	public String chiTietSanPham(@PathVariable int id) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("IoC.xml");
+		DatabaseSanPham db = (DatabaseSanPham) context.getBean("databasesanpham");
+		SanPham sanPham = db.getSanPhamByID(id);
 		return "ChiTietSanPham";
 	}
 }
--- src/main/java/dao/DatabaseCauHinhMay.java
+++ src/main/java/dao/DatabaseCauHinhMay.java
@@ -1,5 +1,17 @@
 package dao;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 public class DatabaseCauHinhMay {
 
+private JdbcTemplate jdbcTemplate;
+	
+	@Autowired
+	public void setDataSource(DataSource dbShopMayTinh) {
+		this.jdbcTemplate = new JdbcTemplate(dbShopMayTinh);
+	}
+	
 }
